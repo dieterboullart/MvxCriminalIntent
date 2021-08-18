6416b5bd1a66ef88070abb783e20af58049688e6
Dieter Boullart <dieter.boullart@rmdy.be>
2021-08-18 14:16:00 +0200

    Initial commit

---
+++ src/MvxCriminalIntent.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using MvxCriminalIntent.Core.ViewModels.Main;
+
+namespace MvxCriminalIntent.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<MainViewModel>();
+        }
+    }
+}
---
+++ src/MvxCriminalIntent.iOS/AppDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Foundation;
+using MvvmCross.Platforms.Ios.Core;
+using MvxCriminalIntent.Core;
+
+namespace MvxCriminalIntent.iOS
+{
+    [Register(nameof(AppDelegate))]
+    public class AppDelegate : MvxApplicationDelegate<Setup, App>
+    {
+    }
+}
